8e5741e37cd7ce4e6bd569444b36da1f6ccc82b1
Phillip Kelly <phillipbentonkelly@gmail.com>
2023-04-04 18:37:37 -0400

    Shared Components(Setup): Change App Name - fix some dependencies

--- src/wrappers.tsx
+++ src/wrappers.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 type Children = React.ReactNode;
 
-export function CenterWrapper(props: { children: Children }): JSX.Element {
-  return <div style={CENTER_STYLING}>{props.children}</div>;
+export function CenterWrapper( props: { children: Children } ): JSX.Element {
+  return (
+    <div style={CENTER_STYLING}>
+      { props.children }    
+    </div>
+  );
 }
 
-export function NarrowWrapper(props: { children: Children }): JSX.Element {
-  return <div style={NARROW_STYLING}>{props.children}</div>;
+export function NarrowWrapper( props: { children: Children } ): JSX.Element {
+  return (
+    <div style={NARROW_STYLING}>
+      { props.children }    
+    </div>
+  );
 }
 
 const CENTER_STYLING = { margin: '2rem auto' };
